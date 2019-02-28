83e74e50b5cbc7fce3e32b63a31ef0f8d4e97d03
Guillaume Blaise <heyfromguillaume@gmail.com>
2019-02-28 18:04:55 +0100

    Initial commit

---
+++ HashCode19/HashCode19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashCode19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
