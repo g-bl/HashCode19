5687e8b8cab22afcc86dedad75652524e5fd2167
sblaise <s.blaise06@gmail.com>
2019-02-28 18:27:44 +0100

    Up

--- HashCode19/HashCode19/Program.cs
+++ HashCode19/HashCode19/Program.cs
@@ -11,7 +11,7 @@ namespace HashCode19
     {
         private static string inputFileName = "e_high_bonus.in"; // a_example b_should_be_easy c_no_hurry d_metropolis e_high_bonus
         private static string outputFileName = "e_high_bonus.out";
-        private static char delimiter = ' ';
+        private static char delimiter = ',';
 
 
         static void Main(string[] args)
