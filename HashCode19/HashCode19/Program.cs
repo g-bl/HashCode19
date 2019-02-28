97ec2a9c0cb3b73071c4c1036263a727a9755525
g-bl <heyfromguillaume@gmail.com>
2019-02-28 18:45:46 +0100

    multiple solvers

--- HashCode19/HashCode19/Program.cs
+++ HashCode19/HashCode19/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace HashCode19
 {
     class HashCodeMainClass
@@ -13,43 +14,9 @@ namespace HashCode19
         private static string outputFileName = "e_high_bonus.out";
         private static char delimiter = ' ';
 
-
         static void Main(string[] args)
         {
-
-            /**************************************************************************************
-             *  Input loading
-             **************************************************************************************/
-
-            Debug.WriteLine("Input loading...");
-
-            string inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), inputFileName);
-            string[] lines = File.ReadAllLines(inputFilePath);
-
-            //First Line
-            string[] firstLine = lines[0].Split(delimiter);
-
-            int R = int.Parse(firstLine[0]); // number of rows
-            int C = int.Parse(firstLine[1]); // number of columns
-            int X = int.Parse(firstLine[2]); // number of vehicles
-            int XX = int.Parse(firstLine[3]); // 
-            int XXX = int.Parse(firstLine[4]); // 
-            int XXXX = int.Parse(firstLine[5]); // 
-
-
-            //Content
-            
-            for (int i = 1; i < lines.Length; i++)
-            {
-                string[] splittedLine = lines[i].Split(delimiter);
-
-                
-            }
-
-
-            /**************************************************************************************
-             *  Solver
-             **************************************************************************************/
+            GBLSolver solver = new GBLSolver(inputFileName, outputFileName, delimiter);
         }
     }
 }
---
+++ HashCode19/HashCode19/SBLSolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode19
+{
+    class SBLSolver
+    {
+        public SBLSolver(string inputFileName, string outputFileName, char delimiter)
+        {
+            /**************************************************************************************
+             *  Input loading
+             **************************************************************************************/
+
+            Debug.WriteLine("Input loading...");
+
+            string inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), inputFileName);
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            //First Line
+            string[] firstLine = lines[0].Split(delimiter);
+
+            int R = int.Parse(firstLine[0]); // number of rows
+            int C = int.Parse(firstLine[1]); // number of columns
+            int X = int.Parse(firstLine[2]); // number of vehicles
+            int XX = int.Parse(firstLine[3]); // 
+            int XXX = int.Parse(firstLine[4]); // 
+            int XXXX = int.Parse(firstLine[5]); // 
+
+            //Content
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] splittedLine = lines[i].Split(delimiter);
+
+
+            }
+
+            /**************************************************************************************
+             *  Solver
+             **************************************************************************************/
+        }
+
+    }
+}
